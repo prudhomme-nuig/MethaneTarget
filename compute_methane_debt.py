b6ea711012647243509a0ab2ebba3eb4ab2a6228
prudhomme-nuig <54855085+prudhomme-nuig@users.noreply.github.com>
2020-05-11 19:57:58 +0200

    Minor change

--- compute_methane_debt.py
+++ compute_methane_debt.py
@@ -13,8 +13,8 @@ from common_data import read_FAOSTAT_df
 country_list=["Brazil","France","India","Ireland"]
 reference_year=2010
 
-def compute_methane_debt(past_trend,year_list,target_year=reference_year):
-    year_mask=year_list<=target_year
+def compute_methane_debt(past_trend,year_list,target_year=reference_year,first_year=1961):
+    year_mask=(year_list<=target_year) & (year_list>=first_year)
     methane_debt=np.sum(past_trend[year_mask])*0.1 #10% of methane long term effect
     return methane_debt
 
@@ -26,9 +26,9 @@ for country in methane_pivot_df['Value'].columns:
     methane_past=methane_pivot_df['Value'][country][~np.isnan(methane_pivot_df['Value'][country])].values
     year_list=methane_pivot_df['Year'][country][~np.isnan(methane_pivot_df['Year'][country])].values
     methane_debt_country=compute_methane_debt(methane_past,year_list)
-    methane_debt_df=pd.concat([methane_debt_df,pd.DataFrame(data={country: [methane_debt_country]})],axis=1)
+    methane_debt_df=pd.concat([methane_debt_df,pd.DataFrame(data={country: [-methane_debt_country/40]})],axis=1)
 
 total_methane_debt=np.sum(methane_debt_df.values)
-methane_debt_df=methane_debt_df[country_list]/total_methane_debt
-
-methane_debt_df.to_csv('output/FAOSTAT_methane_debt.csv',index=False)
+methane_debt_share_df=methane_debt_df[country_list]/total_methane_debt
+methane_debt_share_df.to_csv('output/FAOSTAT_methane_debt.csv',index=False)
+methane_debt_df.to_csv('output/FAOSTAT_methane_debt_value.csv',index=False)

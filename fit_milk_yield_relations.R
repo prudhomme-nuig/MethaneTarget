d15ae32967508e8568431283960cd693c3917eb3
prudhomme-nuig <remiprudhomme@hotmail.fr>
2020-08-10 14:59:53 +0200

    Same relation for tropical and temperate countries with a fixed effect per GAEZ zone.

--- fit_milk_yield_relations.R
+++ fit_milk_yield_relations.R
@@ -22,27 +22,34 @@ ggplot(df_temperate,aes(x=Concentrate_intake, y=Milk_yield,color=factor(GAEZ),sh
         legend.text = element_text(size = 18),
         legend.title = element_text(face = "bold",size = 18))
 
-df_tropical<-df[df["GAEZ"]=="Tropical",]
-mod_tropical <- lm(Milk_yield ~ poly(Concentrate_intake,2)  , data=df_tropical)
-#mod <- lm(Milk_yield ~ Concentrate_intake + Grass_intake, data=df_tropical)
-summary(mod_tropical)
-par(mfrow = c(2, 2))
-plot(mod_tropical)
-df_tropical$predlm <- predict(mod_tropical)
+library(e1071)
 
+saveRDS(mod_temparate, "output/milk_yield_concentrate.rds")
+file_conn <- file("output/milk_yield_concentrate.dep")
+writeLines(DEP_LIBS, file_conn)
+close(file_conn)
 
-ggplot(df_tropical,aes(x=Concentrate_intake, y=Milk_yield,color=factor(Grass_intake_cat))) +
-  geom_point(size=3) + 
-  geom_line(aes(y = predlm), size = 1.5)+
-  scale_color_discrete(name = "Grass intake:", labels = c("1 tDM/year", "2 tDM/year", "3 tDM/year"))+
-  labs(y="Milk yield (t/year)", x = "Concentrate intake (tDM/year)")+
-  theme(axis.title = element_text(size = 18),
-        legend.text = element_text(size = 18),
-        legend.title = element_text(face = "bold",size = 18))
-
-library("plyr")
-coef_final<-rbind.fill(as.data.frame.list(mod_temparate$coefficients),as.data.frame.list(mod_tropical$coefficients))
-colnames(coef_final)<- c("Intercept","Concentrate_intake","Concentrate_intake_2","Grass_intake_cat.3","factor.Grass_intake_cat.4")
-rownames(coef_final)<-c("Temparate","Tropical")
-coef_final[is.na(coef_final)] <- 0
-write.csv(coef_final,'output/coefficients_milk_yield_concentrate_relation.csv')
+# df_tropical<-df[df["GAEZ"]=="Tropical",]
+# mod_tropical <- lm(Milk_yield ~ poly(Concentrate_intake,2)  , data=df_tropical)
+# #mod <- lm(Milk_yield ~ Concentrate_intake + Grass_intake, data=df_tropical)
+# summary(mod_tropical)
+# par(mfrow = c(2, 2))
+# plot(mod_tropical)
+# df_tropical$predlm <- predict(mod_tropical)
+# 
+# 
+# ggplot(df_tropical,aes(x=Concentrate_intake, y=Milk_yield,color=factor(Grass_intake_cat))) +
+#   geom_point(size=3) + 
+#   geom_line(aes(y = predlm), size = 1.5)+
+#   scale_color_discrete(name = "Grass intake:", labels = c("1 tDM/year", "2 tDM/year", "3 tDM/year"))+
+#   labs(y="Milk yield (t/year)", x = "Concentrate intake (tDM/year)")+
+#   theme(axis.title = element_text(size = 18),
+#         legend.text = element_text(size = 18),
+#         legend.title = element_text(face = "bold",size = 18))
+# 
+# library("plyr")
+# coef_final<-rbind.fill(as.data.frame.list(mod_temparate$coefficients),as.data.frame.list(mod_tropical$coefficients))
+# colnames(coef_final)<- c("Intercept","Concentrate_intake","Concentrate_intake_2","Grass_intake_cat.3","factor.Grass_intake_cat.4")
+# rownames(coef_final)<-c("Temparate","Tropical")
+# coef_final[is.na(coef_final)] <- 0
+# write.csv(coef_final,'output/coefficients_milk_yield_concentrate_relation.csv')
